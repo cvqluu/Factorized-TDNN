8538c0b7ab6786c62a974cefa45cbae9d527e8e9
Chau Luu <s1786813@sms.ed.ac.uk>
2019-11-20 15:34:49 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
 # Factorized-TDNN
-Pytorch implementation of the Factorized TDNN (TDNN-F) from "Semi-Orthogonal Low-Rank Matrix Factorization for Deep Neural Networks"
+
+PyTorch implementation of the Factorized TDNN (TDNN-F) from ["Semi-Orthogonal Low-Rank Matrix Factorization for Deep Neural Networks"](http://danielpovey.com/files/2018_interspeech_tdnnf.pdf)[1]. This is also known as TDNN-F in nnet3 of [Kaldi](https://github.com/kaldi-asr/kaldi).
+
+![model_fig](figures/ftdnn.png?raw=true "ftdnn diag") Taken from [1]
+
+A TDNN-F layer is implemented in the class `FTDNNLayer` of `models.py`. To be specific to the description in [1], it is an implementation of the **"3-stage splicing"** implementation, in which three convolutions are used in sequence, with the first two being constrained to be semi-orthogonal. These convolutions are followed by a ReLU and then BatchNorm layer. The semi-orthogonal constraint is the **"floating case"** in [1]. (TODO: implement the scaled case like in Kaldi)
+
+# Usage
+
+This `FTDNNLayer` of `models.py` is used as follows:
+
+```python
+import torch
+from models import FTDNNLayer
+
+tdnn_f = FTDNNLayer(1280, 512, 256, context_size=2, dilations=[2,2,2], paddings=[1,1,1])
+# This is a sequence of three 2x1 convolutions
+# dimensions go from 1280 -> 256 -> 256 -> 512
+# dilations and paddings handles how much to dilate and pad each convolution
+# Having these configurable is to ensure the sequence length stays the same
+
+test_input = torch.rand(5, 100, 1280)
+# inputs to the FTDNNLayer must be (batch_size, seq_len, in_dim)
+
+tdnn_f(test_input).shape # returns (5, 100, 512)
+
+tdnn_f.step_semi_orth() # The key method to constrain the first two convolutions, perform after every SGD step
+
+tdnn_f.orth_error() # This returns the frobenius norm of the two constrained convs, useful for debugging
+```
+
+The implementation of the `.step_semi_orth()` method has been made to be as close to `ConstrainOrthonormalInternal` from [nnet-utils.cc](https://github.com/kaldi-asr/kaldi/blob/master/src/nnet3/nnet-utils.cc) in Kaldi's `nnet3` module.
+
+# Extras
+
+Also included in this repo in `models.py` is the following:
+ * `FTDNN`: Factorized TDNN x-vector architecture (FTDNN) seen in  ["State-of-the-art speaker recognition with neural network embeddings in NIST SRE18 and Speakers in the Wild evaluations"](https://www.sciencedirect.com/science/article/pii/S0885230819302700)[2]. (This is not EXACTLY the same, but should be close enough).
+ * `SharedDimScaleDropout`: The shared dimension scaled dropout described in [1] and in Kaldi:
+     * Instead of randomly setting inputs to 0, use a continuous dropout scale.
+     * For a dropout 'strength' alpha, multiply inputs inputs by a mask sampled from the uniform distribution on the interval [1 - 2 \* alpha, 1 + 2 \* alpha].
+     * Share dropout masks along a dimension, such as time. From [1]: "If, for instance, a dimension is zeroed on a particular frame it will be zeroed on all frames of that sequence".
+
+
+An demonstration of the `FTDNN` model being trained can be seen in the following output log (code not included, TODO: basic experiment demo):
+
+```
+exp/sp_ftdnn_bl: Wed Nov 20 14:21:15 2019: [10/120000]   C-Loss:21.9116, AvgLoss:21.6991, lr: 0.2, bs: 400
+Orth error: 22.44341427081963
+exp/sp_ftdnn_bl: Wed Nov 20 14:21:29 2019: [20/120000]   C-Loss:21.6260, AvgLoss:21.7459, lr: 0.2, bs: 400
+Orth error: 8.235212338215206
+exp/sp_ftdnn_bl: Wed Nov 20 14:21:43 2019: [30/120000]   C-Loss:21.7663, AvgLoss:21.7525, lr: 0.2, bs: 400
+Orth error: 1.2611256236341433
+exp/sp_ftdnn_bl: Wed Nov 20 14:21:56 2019: [40/120000]   C-Loss:21.6153, AvgLoss:21.6527, lr: 0.2, bs: 400
+Orth error: 0.005309408872562926
+exp/sp_ftdnn_bl: Wed Nov 20 14:22:14 2019: [50/120000]   C-Loss:21.0997, AvgLoss:21.5722, lr: 0.2, bs: 400
+Orth error: 0.005543942232179688
+exp/sp_ftdnn_bl: Wed Nov 20 14:22:26 2019: [60/120000]   C-Loss:21.2629, AvgLoss:21.5222, lr: 0.2, bs: 400
+Orth error: 0.004769200691953301
+exp/sp_ftdnn_bl: Wed Nov 20 14:22:40 2019: [70/120000]   C-Loss:20.9551, AvgLoss:21.4158, lr: 0.2, bs: 400
+Orth error: 0.006055477493646322
+exp/sp_ftdnn_bl: Wed Nov 20 14:22:56 2019: [80/120000]   C-Loss:20.4425, AvgLoss:21.3274, lr: 0.2, bs: 400
+Orth error: 0.009634702852054033
+exp/sp_ftdnn_bl: Wed Nov 20 14:23:09 2019: [90/120000]   C-Loss:21.0025, AvgLoss:21.2727, lr: 0.2, bs: 400
+Orth error: 0.00611297079740325
+exp/sp_ftdnn_bl: Wed Nov 20 14:23:25 2019: [100/120000]          C-Loss:20.6145, AvgLoss:21.1736, lr: 0.2, bs: 400
+Orth error: 0.008151484609697945
+exp/sp_ftdnn_bl: Wed Nov 20 14:23:38 2019: [110/120000]          C-Loss:20.1985, AvgLoss:21.0890, lr: 0.2, bs: 400
+Orth error: 0.0072971017434610985
+exp/sp_ftdnn_bl: Wed Nov 20 14:23:53 2019: [120/120000]          C-Loss:20.5698, AvgLoss:21.0300, lr: 0.2, bs: 400
+Orth error: 0.00629939052669215
+exp/sp_ftdnn_bl: Wed Nov 20 14:24:08 2019: [130/120000]          C-Loss:20.2024, AvgLoss:20.9425, lr: 0.2, bs: 400
+Orth error: 0.008707787481398555
+exp/sp_ftdnn_bl: Wed Nov 20 14:24:21 2019: [140/120000]          C-Loss:19.7034, AvgLoss:20.8641, lr: 0.2, bs: 400
+Orth error: 0.010941843771433923
+exp/sp_ftdnn_bl: Wed Nov 20 14:24:37 2019: [150/120000]          C-Loss:19.9718, AvgLoss:20.8035, lr: 0.2, bs: 400
+Orth error: 0.00768740743296803
+```
+
+The FTDNN x-vector architecture seems to train successfully, and most importantly the Orth error is minimized.
+
+# References
+
+```
+[1]
+@inproceedings{Povey2018,
+  author={Daniel Povey and Gaofeng Cheng and Yiming Wang and Ke Li and Hainan Xu and Mahsa Yarmohammadi and Sanjeev Khudanpur},
+  title={Semi-Orthogonal Low-Rank Matrix Factorization for Deep Neural Networks},
+  year=2018,
+  booktitle={Proc. Interspeech 2018},
+  pages={3743--3747},
+  doi={10.21437/Interspeech.2018-1417},
+  url={http://dx.doi.org/10.21437/Interspeech.2018-1417}
+}
+```
+
+```
+[2]
+@article{VILLALBA2020101026,
+    title = "State-of-the-art speaker recognition with neural network embeddings in NIST SRE18 and Speakers in the Wild evaluations",
+    journal = "Computer Speech & Language",
+    volume = "60",
+    pages = "101026",
+    year = "2020",
+    issn = "0885-2308",
+    doi = "https://doi.org/10.1016/j.csl.2019.101026",
+    url = "http://www.sciencedirect.com/science/article/pii/S0885230819302700",
+    author = "Jesús Villalba and Nanxin Chen and David Snyder and Daniel Garcia-Romero and Alan McCree and Gregory Sell and Jonas Borgstrom and Leibny Paola García-Perera and Fred Richardson and Réda Dehak and Pedro A. Torres-Carrasquillo and Najim Dehak"
+}
+```
---
+++ models.py
@@ -0,0 +1,297 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FTDNNLayer(nn.Module):
+    
+    def __init__(self, in_dim, out_dim, bottleneck_dim, context_size=2, dilations=None, paddings=None, alpha=0.0):
+        '''
+        3 stage factorised TDNN http://danielpovey.com/files/2018_interspeech_tdnnf.pdf
+        '''
+        super(FTDNNLayer, self).__init__()
+        paddings = [1,1,1] if not paddings else paddings
+        dilations = [2,2,2] if not dilations else dilations
+        assert len(paddings) == 3
+        assert len(dilations) == 3
+        kwargs = {'bias':False}
+        self.factor1 = nn.Conv1d(in_dim, bottleneck_dim, context_size, padding=paddings[0], dilation=dilations[0], **kwargs)
+        self.factor2 = nn.Conv1d(bottleneck_dim, bottleneck_dim, context_size, padding=paddings[1], dilation=dilations[1], **kwargs)
+        self.factor3 = nn.Conv1d(bottleneck_dim, out_dim, context_size, padding=paddings[2], dilation=dilations[2], **kwargs)
+        self.reset_parameters()
+        self.nl = nn.ReLU()
+        self.bn = nn.BatchNorm1d(out_dim)
+        self.dropout = SharedDimScaleDropout(alpha=alpha, dim=1)
+        
+    def forward(self, x):
+        ''' input (batch_size, seq_len, in_dim) '''
+        assert (x.shape[-1] == self.factor1.weight.shape[1])
+        x = self.factor1(x.transpose(1,2))
+        x = self.factor2(x)
+        x = self.factor3(x)
+        x = self.nl(x)
+        x = self.bn(x).transpose(1,2)
+        x = self.dropout(x)
+        return x
+    
+    def step_semi_orth(self):
+        with torch.no_grad():
+            factor1_M = self.get_semi_orth_weight(self.factor1)
+            factor2_M = self.get_semi_orth_weight(self.factor2)
+            self.factor1.weight.copy_(factor1_M)
+            self.factor2.weight.copy_(factor2_M)
+    
+    def reset_parameters(self):
+        # Standard dev of M init values is inverse of sqrt of num cols
+        nn.init._no_grad_normal_(self.factor1.weight, 0., self.get_M_shape(self.factor1.weight)[1]**-0.5)
+        nn.init._no_grad_normal_(self.factor2.weight, 0., self.get_M_shape(self.factor2.weight)[1]**-0.5)
+        
+    def orth_error(self):
+        factor1_err = self.get_semi_orth_error(self.factor1).item()
+        factor2_err = self.get_semi_orth_error(self.factor2).item()
+        return factor1_err + factor2_err
+
+    @staticmethod
+    def get_semi_orth_weight(conv1dlayer):
+        # updates conv1 weight M using update rule to make it more semi orthogonal
+        # based off ConstrainOrthonormalInternal in nnet-utils.cc in Kaldi src/nnet3
+        # includes the tweaks related to slowing the update speed
+        # only an implementation of the 'floating scale' case
+        with torch.no_grad():
+            update_speed = 0.125
+            orig_shape = conv1dlayer.weight.shape
+            # a conv weight differs slightly from TDNN formulation:
+            # Conv weight: (out_filters, in_filters, kernel_width)
+            # TDNN weight M is of shape: (in_dim, out_dim) or [rows, cols]
+            # the in_dim of the TDNN weight is equivalent to in_filters * kernel_width of the Conv
+            M = conv1dlayer.weight.reshape(orig_shape[0], orig_shape[1]*orig_shape[2]).T
+            # M now has shape (in_dim[rows], out_dim[cols])
+            mshape = M.shape
+            if mshape[0] > mshape[1]: # semi orthogonal constraint for rows > cols
+                M = M.T
+            P = torch.mm(M, M.T)
+            PP = torch.mm(P, P.T)
+            trace_P = torch.trace(P)
+            trace_PP = torch.trace(PP)
+            ratio = trace_PP * P.shape[0] / (trace_P * trace_P)
+
+            # the following is the tweak to avoid divergence (more info in Kaldi)
+            assert ratio > 0.999
+            if ratio > 1.02:
+                update_speed *= 0.5
+                if ratio > 1.1:
+                    update_speed *= 0.5
+
+            scale2 = trace_PP/trace_P
+            update = P - (torch.matrix_power(P, 0) * scale2)
+            alpha = update_speed / scale2
+            update = (-4.0 * alpha) * torch.mm(update, M)
+            updated = M + update
+            # updated has shape (cols, rows) if rows > cols, else has shape (rows, cols)
+            # Transpose (or not) to shape (cols, rows) (IMPORTANT, s.t. correct dimensions are reshaped)
+            # Then reshape to (cols, in_filters, kernel_width)
+            return updated.reshape(*orig_shape) if mshape[0] > mshape[1] else updated.T.reshape(*orig_shape)
+
+    @staticmethod
+    def get_M_shape(conv_weight):
+        orig_shape = conv_weight.shape
+        return (orig_shape[1]*orig_shape[2], orig_shape[0])
+
+    @staticmethod
+    def get_semi_orth_error(conv1dlayer):
+        with torch.no_grad():
+            orig_shape = conv1dlayer.weight.shape
+            M = conv1dlayer.weight.reshape(orig_shape[0], orig_shape[1]*orig_shape[2])
+            mshape = M.shape
+            if mshape[0] > mshape[1]: # semi orthogonal constraint for rows > cols
+                M = M.T
+            P = torch.mm(M, M.T)
+            PP = torch.mm(P, P.T)
+            trace_P = torch.trace(P)
+            trace_PP = torch.trace(PP)
+            ratio = trace_PP * P.shape[0] / (trace_P * trace_P)
+            scale2 = torch.sqrt(trace_PP/trace_P) ** 2
+            update = P - (torch.matrix_power(P, 0) * scale2)
+            return torch.norm(update, p='fro')
+
+
+class DenseReLU(nn.Module):
+    
+    def __init__(self, in_dim, out_dim):
+        super(DenseReLU, self).__init__()
+        self.fc = nn.Linear(in_dim, out_dim)
+        self.bn = nn.BatchNorm1d(out_dim)
+        self.nl = nn.ReLU()
+    
+    def forward(self, x):
+        x = self.fc(x)
+        x = self.nl(x)
+        if len(x.shape) > 2:
+            x = self.bn(x.transpose(1,2)).transpose(1,2)
+        else:
+            x = self.bn(x)
+        return x
+
+
+class StatsPool(nn.Module):
+
+    def __init__(self, floor=1e-10, bessel=False):
+        super(StatsPool, self).__init__()
+        self.floor = floor
+        self.bessel = bessel
+
+    def forward(self, x):
+        means = torch.mean(x, dim=1)
+        _, t, _ = x.shape
+        if self.bessel:
+            t = t - 1
+        residuals = x - means.unsqueeze(1)
+        numerator = torch.sum(residuals**2, dim=1)
+        stds = torch.sqrt(torch.clamp(numerator, min=self.floor)/t)
+        x = torch.cat([means, stds], dim=1)
+        return x
+
+
+class TDNN(nn.Module):
+    
+    def __init__(
+                    self, 
+                    input_dim=23, 
+                    output_dim=512,
+                    context_size=5,
+                    stride=1,
+                    dilation=1,
+                    batch_norm=True,
+                    dropout_p=0.0,
+                    padding=0
+                ):
+        super(TDNN, self).__init__()
+        self.context_size = context_size
+        self.stride = stride
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.dilation = dilation
+        self.dropout_p = dropout_p
+        self.padding = padding
+        
+        self.kernel = nn.Conv1d(self.input_dim, 
+                                self.output_dim,
+                                self.context_size, 
+                                stride=self.stride, 
+                                padding=self.padding, 
+                                dilation=self.dilation)
+
+        self.nonlinearity = nn.LeakyReLU()
+        self.batch_norm = batch_norm
+        if batch_norm:
+            self.bn = nn.BatchNorm1d(output_dim)
+        self.drop = nn.Dropout(p=self.dropout_p)
+        
+    def forward(self, x):
+        '''
+        input: size (batch, seq_len, input_features)
+        outpu: size (batch, new_seq_len, output_features)
+        '''
+
+        _, _, d = x.shape
+        assert (d == self.input_dim), 'Input dimension was wrong. Expected ({}), got ({})'.format(self.input_dim, d)
+        
+        x = self.kernel(x.transpose(1,2))
+        x = self.nonlinearity(x)
+        x = self.drop(x)
+
+        if self.batch_norm:           
+            x = self.bn(x)
+        return x.transpose(1,2)
+        
+
+class FTDNN(nn.Module):
+    
+    def __init__(self, in_dim=30):
+        '''
+        The FTDNN architecture from
+        "State-of-the-art speaker recognition with neural network embeddings in 
+        NIST SRE18 and Speakers in the Wild evaluations"
+        https://www.sciencedirect.com/science/article/pii/S0885230819302700
+        '''
+        super(FTDNN, self).__init__()
+        
+        self.layer01 = TDNN(input_dim=in_dim, output_dim=512, context_size=5, padding=2)
+        self.layer02 = FTDNNLayer(512, 1024, 256, context_size=2, dilations=[2,2,2], paddings=[1,1,1])
+        self.layer03 = FTDNNLayer(1024, 1024, 256, context_size=1, dilations=[1,1,1], paddings=[0,0,0])
+        self.layer04 = FTDNNLayer(1024, 1024, 256, context_size=2, dilations=[3,3,2], paddings=[2,1,1])
+        self.layer05 = FTDNNLayer(2048, 1024, 256, context_size=1, dilations=[1,1,1], paddings=[0,0,0])
+        self.layer06 = FTDNNLayer(1024, 1024, 256, context_size=2, dilations=[3,3,2], paddings=[2,1,1])
+        self.layer07 = FTDNNLayer(3072, 1024, 256, context_size=2, dilations=[3,3,2], paddings=[2,1,1])
+        self.layer08 = FTDNNLayer(1024, 1024, 256, context_size=2, dilations=[3,3,2], paddings=[2,1,1])
+        self.layer09 = FTDNNLayer(3072, 1024, 256, context_size=1, dilations=[1,1,1], paddings=[0,0,0])
+        self.layer10 = DenseReLU(1024, 2048)
+        
+        self.layer11 = StatsPool()
+        
+        self.layer12 = DenseReLU(4096, 512)
+        
+        
+    def forward(self, x):
+        '''
+        Input must be (batch_size, seq_len, in_dim)
+        '''
+        x = self.layer01(x)
+        x_2 = self.layer02(x)
+        x_3 = self.layer03(x_2)
+        x_4 = self.layer04(x_3)
+        skip_5 = torch.cat([x_4, x_3], dim=-1)
+        x = self.layer05(skip_5)
+        x_6 = self.layer06(x)
+        skip_7 = torch.cat([x_6, x_4, x_2], dim=-1)
+        x = self.layer07(skip_7)
+        x_8 = self.layer08(x)
+        skip_9 = torch.cat([x_8, x_6, x_4], dim=-1)
+        x = self.layer09(skip_9)
+        x = self.layer10(x)
+        x = self.layer11(x)
+        x = self.layer12(x)
+        return x
+
+    def step_ftdnn_layers(self):
+        for layer in self.children():
+            if isinstance(layer, FTDNNLayer):
+                layer.step_semi_orth()
+
+    def set_dropout_alpha(self, alpha):
+        for layer in self.children():
+            if isinstance(layer, FTDNNLayer):
+                layer.dropout.alpha = alpha
+
+    def get_orth_errors(self):
+        errors = 0.
+        with torch.no_grad():
+            for layer in self.children():
+                if isinstance(layer, FTDNNLayer):
+                    errors += layer.orth_error()
+        return errors
+                
+
+class SharedDimScaleDropout(nn.Module):
+    def __init__(self, alpha: float = 0.5, dim=1):
+        '''
+        Continuous scaled dropout that is const over chosen dim (usually across time)
+        Multiplies inputs by random mask taken from Uniform([1 - 2\alpha, 1 + 2\alpha])
+        '''
+        super(SharedDimScaleDropout, self).__init__()
+        if alpha > 0.5 or alpha < 0:
+            raise ValueError("alpha must be between 0 and 0.5")
+        self.alpha = alpha
+        self.dim = dim
+        self.register_buffer('mask', torch.tensor(0.))
+         
+    def forward(self, X):
+        if self.training:
+            if self.alpha != 0.:
+                # sample mask from uniform dist with dim of length 1 in self.dim and then repeat to match size
+                tied_mask_shape = list(X.shape)
+                tied_mask_shape[self.dim] = 1
+                repeats = [1 if i != self.dim else X.shape[self.dim] for i in range(len(X.shape))]
+                return X * self.mask.repeat(tied_mask_shape).uniform_(1 - 2*self.alpha, 1 + 2*self.alpha).repeat(repeats)
+                # expected value of dropout mask is 1 so no need to scale outputs like vanilla dropout
+        return X
